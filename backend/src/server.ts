0bf3454aaf17d9881d31154ea117ba756ae2b3f6
Suraj Kushwaha <119525498+suraj371k@users.noreply.github.com>
2026-04-14 19:13:28 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -48,7 +48,7 @@ app.post(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
-app.use(cors({ origin: process.env.CORS_ORIGIN!, credentials: true }));
+app.use(cors({ origin: "https://code-vault-alpha-nine.vercel.app", credentials: true }));
 app.use(cookieParser());
 
 //health route
