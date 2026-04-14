ac8d113c4b4d272cbaba807a5d5a84d4a1dc5a1d
suraj371k <surajkushwaha371@gmail.com>
2026-04-14 18:09:41 +0530

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -45,7 +45,7 @@ app.post(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(cookieParser());
 
 //health route
